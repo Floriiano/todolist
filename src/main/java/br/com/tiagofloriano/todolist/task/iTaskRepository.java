f85d11ad7d685e9a32734979db4af05e03d22638
Tiago Floriano de Lima <tiago_damg@hotmail.com>
2023-10-15 11:46:30 -0300

    Todolist and Dockerfile

---
+++ src/main/java/br/com/tiagofloriano/todolist/task/iTaskRepository.java
@@ -0,0 +1,10 @@
+package br.com.tiagofloriano.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface iTaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+}
---
+++ src/main/java/br/com/tiagofloriano/todolist/user/iUserRepository.java
@@ -0,0 +1,8 @@
+package br.com.tiagofloriano.todolist.user;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface iUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+}
